435e11bc30d4c6047ad22df5c41444d4077347dc
Filirien <filirien@abv.bg>
2018-08-09 00:27:13 +0300

    Initial commit

---
+++ StarCraftNews.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StarCraftNews.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
